b8562c385a6c19147aaa7f7ae103315a0db7de2f
rhw <ruohanw89@gmail.com>
2020-08-01 21:32:51 -0600

    mocks test with potion added to player

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -1,4 +1,7 @@
 const Player = require("../lib/Player.js");
+const Potion = require("../lib/Potion");
+
+jest.mock("../lib/Potion");
 
 test("creates a player object", () => {
   const player = new Player("Dave");
@@ -7,4 +10,8 @@ test("creates a player object", () => {
   expect(player.health).toEqual(expect.any(Number));
   expect(player.strength).toEqual(expect.any(Number));
   expect(player.agility).toEqual(expect.any(Number));
+
+  expect(player.inventory).toEqual(
+    expect.arrayContaining([expect.any(Object)])
+  );
 });
